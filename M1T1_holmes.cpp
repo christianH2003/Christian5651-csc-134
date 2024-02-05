d863c3939c0874d94c2a6b3f6268c579f54fe0dd
christianH2003 <158074326+christianH2003@users.noreply.github.com>
2024-02-05 21:51:31 +0000

    M1T1 complete

---
+++ M1T1_holmes.cpp
@@ -0,0 +1,16 @@
+/*
+csc 134
+M1T1 - Hello World 
+Christian Holmes
+ 2/4/24
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    string name = "Your Name";
+    cout << " nice to meet you, " << name  << endl; 
+    return 0;
+}
