e7a227f380730031fb75e79b34777748126ea19f
christianH2003 <158074326+christianH2003@users.noreply.github.com>
2024-02-05 22:10:38 +0000

    M1T2 Complete

---
+++ M1T2_holmes.cpp
@@ -0,0 +1,16 @@
+/*
+csc 134
+M1T1 - Hello World 
+Christian Holmes
+ 2/4/24
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << " My name is Christian Holmes" << endl; 
+    cout << " I am studying to beccome a machanical Engineer" << endl; 
+    cout << " I like to play airsoft in my free time" << endl; 
+    return 0;
+}
